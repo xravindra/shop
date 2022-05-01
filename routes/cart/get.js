a271395264acfdf92a1d96e4cb7807a7b49e5fbf
Ravindra <ravindra@Ravindras-MacBook-Air.local>
2022-05-01 17:08:23 +0530

    opts cart

--- routes/cart/get.js
+++ routes/cart/get.js
@@ -1,15 +1,16 @@
 const express = require("express")
 const router = express.Router()
-const Product = require("../../models/Product")
 const Cart = require("../../models/Cart")
 
 router.get("/", async (req, res) => {
   try {
     const userId = req.query.userId
-    const cart = await Cart.find(userId ? { user: userId } : {}).populate('product')
+    const cart = await Cart
+      .find(userId ? { user: userId } : {})
+      .populate('product')
 
     if (cart) {
-      return res.send(cart)
+      return res.send(cart.map(c => c.product))
     }
 
     throw ({
