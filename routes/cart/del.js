846addcc852a3ff37c841632e96bfb4e480fc279
Ravindra <ravindra@Ravindras-MacBook-Air.local>
2022-05-01 17:16:38 +0530

    delete cart item

--- routes/cart/del.js
+++ routes/cart/del.js
@@ -1,12 +1,11 @@
 const express = require("express")
 const router = express.Router()
-const Token = require("../../models/Token");
+const Cart = require("../../models/Cart");
 
-// logout the user
 router.delete("/", async (req, res) => {
   try {
-    const token = req.body.token
-    await Token.deleteOne({ token })
+    const { userId, productId } = req.body
+    await Cart.deleteOne({ user: userId, product: productId })
     return res.sendStatus(204)
   } catch (error) { return res.send(error) }
 })
