defd112d915a799b902fab29c3fbc6438e7f7a17
Ravindra <ravindra@Ravindras-MacBook-Air.local>
2022-05-09 15:54:25 +0530

    deleter

--- routes/order/del.js
+++ routes/order/del.js
@@ -4,8 +4,8 @@ const Cart = require("../../models/Cart");
 
 router.delete("/", async (req, res) => {
   try {
-    const { userId, productId } = req.body
-    await Cart.deleteOne({ user: userId, product: productId })
+    const { userId } = req.body
+    await Cart.deleteMany({ user: userId })
     return res.sendStatus(204)
   } catch (error) { return res.send(error) }
 })
